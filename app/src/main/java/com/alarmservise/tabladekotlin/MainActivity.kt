e9cd13ae99a6b9673c90ed1d6226d864853a946b
Андрей Кузнецов <alarmservise@mail.ru>
2021-09-22 10:11:45 +0300

    finish 4 lesson

--- app/src/main/java/com/alarmservise/tabladekotlin/MainActivity.kt
+++ app/src/main/java/com/alarmservise/tabladekotlin/MainActivity.kt
@@ -6,22 +6,26 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import com.alarmservise.tabladekotlin.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
 
 class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+
+    private lateinit var rootElement:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        val view = rootElement.root
+        setContentView(view)
         init()
     }
 
     private fun init(){
-        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
-        drawerLayout.addDrawerListener(toggle)
+        val toggle = ActionBarDrawerToggle(this,rootElement.drawerLayout,rootElement.mainContent.toolbar,R.string.open,R.string.close)
+        rootElement.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        navView.setNavigationItemSelectedListener (this)
+        rootElement.navView.setNavigationItemSelectedListener (this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -52,7 +56,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
             }
 
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
+        rootElement.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
